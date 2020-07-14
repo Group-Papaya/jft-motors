51d8035e4c2ce471adf0276e1a4595311b975e12
Ian Mubangizi <io@ianmubangizi.com>
2020-07-14 10:49:49 +0200

    added create & delete with client service

---
+++ tests/unit/client.service.spec.ts
@@ -0,0 +1,49 @@
+import { Client } from "@/models";
+import clientService from "@/services/client.service";
+import { firestore, firestoreSetup, firestoreTeardown } from "./setup-teardown";
+
+const { saveClientAsync, deleteClientAsync } = clientService;
+
+let client: Client;
+let saved: Client;
+
+beforeAll(() => {
+  firestoreSetup();
+  client = new Client();
+  client.email = "test-client@jft-motors.web.app";
+  client.firstname = "test-client";
+  client.lastname = "example";
+});
+
+afterAll(() => {
+  firestoreTeardown();
+});
+
+describe("Client Service", () => {
+  const batch = firestore.batch();
+  const clients = firestore.collection("clients");
+
+  afterEach(async () => {
+    await clients.get().then((snap) => {
+      snap.docs.forEach(async (doc) => await doc.ref.delete());
+    });
+  }, 3000);
+
+  it("can save client async", async () => {
+    await saveClientAsync(client).then((_client) => {
+      expect(_client).not.toBe(null);
+      expect(_client?.id).not.toBe(undefined);
+    });
+  });
+
+  it("can delete client async", async () => {
+    await saveClientAsync(client).then(async (_client) => {
+      expect(client.email).toEqual(_client?.email);
+      if (_client)
+        await deleteClientAsync(_client.id).then(async () => {
+          const doc = await clients.doc(_client.id).get();
+          expect(doc.exists).toBeFalsy();
+        });
+    });
+  });
+});
