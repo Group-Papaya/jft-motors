3336778e06baaa5e644351d6cc18b07e7fbb16d6
Ian Mubangizi <io@ianmubangizi.com>
2020-07-14 06:32:26 +0200

    clean  sudo user create test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,26 +10,6 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
-import { Collection } from "firebase-firestorm";
-import User, { ADMIN_ROLE } from "@/models/User";
-
-const defaulUser = new User();
-
-defaulUser.firstname = "Ian";
-defaulUser.lastname = "Mubangizi";
-defaulUser.password = "password";
-defaulUser.email = "io@ianmubangizi.com";
-defaulUser.role = ADMIN_ROLE;
-
-let user;
-
-Collection(User)
-  .create(defaulUser)
-  .then(_user => {
-    if (_user) user = _user;
-  });
-
-console.log(user);
 
 export default {
   name: "Home",
