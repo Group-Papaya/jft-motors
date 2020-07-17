d220ebc246ff6e829abb0cfeabefb570114017be
Ian Mubangizi <io@ianmubangizi.com>
2020-07-17 17:31:53 +0200

    Fix Eslint Style Issues Fixes #9

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h1>{{ text }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,
       <br />check out the
@@ -17,6 +18,7 @@ import { Component, Prop, Vue } from "vue-property-decorator";
 @Component
 export default class HelloWorld extends Vue {
   @Prop() private msg!: string;
+  @Prop() private text!: string;
 }
 </script>
 
