011608f956f284ecd0257438f1d00ee81f57694e
Ian Mubangizi <io@ianmubangizi.com>
2020-07-18 11:14:06 +0200

    apply plugin

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
 import "./firebase";
-import router from "./routes";
+import "./plugins/base";
+import "./plugins/validate";
+import vuetify from "./plugins/vuetify";
+import router from "./router";
 import "./service-worker";
 import store from "./store";
 
@@ -10,5 +13,6 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
