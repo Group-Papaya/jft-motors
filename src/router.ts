8035f51b7e243829e3b995fa4de022365703d460
Ian Mubangizi <io@ianmubangizi.com>
2020-07-18 11:17:08 +0200

    add new views

---
+++ src/router.ts
@@ -0,0 +1,27 @@
+import Vue from "vue";
+import Router from "vue-router";
+
+Vue.use(Router);
+
+export default new Router({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "/",
+      component: () => import("@/components/layouts/AppLayout.vue"),
+      children: [
+        {
+          name: "Dashboard",
+          path: "",
+          component: () => import("./views/Dashboard.vue")
+        },
+        {
+          name: "User Profile",
+          path: "profile",
+          component: () => import("./views/UserProfile.vue")
+        }
+      ]
+    }
+  ]
+});
--- src/store.ts
+++ src/store.ts
@@ -5,10 +5,19 @@ import { vuexfireMutations } from "vuexfire";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    barColor: "rgba(0, 0, 0, .75), rgba(0, 0, 0, .75)",
+    barImage: require("@/assets/background.jpg"),
+    drawer: null
+  },
   mutations: {
+    SET_BAR_IMAGE(state, payload) {
+      state.barImage = payload;
+    },
+    SET_DRAWER(state, payload) {
+      state.drawer = payload;
+    },
     ...vuexfireMutations
   },
-  actions: {},
-  modules: {}
+  actions: {}
 });
