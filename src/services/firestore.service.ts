938bfe0a497940b5ff538e040052935dac0966b2
Ian Mubangizi <io@ianmubangizi.com>
2020-07-20 22:09:56 +0200

    new firebase test cases with log utils

--- src/services/firestore.service.ts
+++ src/services/firestore.service.ts
@@ -1,25 +1,36 @@
-import { Record } from "@/models";
-import { tryCatch } from "@/utils";
+import { LineItem, Quotation, Record } from "@/models";
+import { DocumentRef } from "@/models/Record";
+import { orLog, tryCatch } from "@/utils";
+
+type Ref = DocumentRef;
 
 export default class {
   firestore!: firebase.firestore.Firestore;
 
-  @tryCatch((error) => {
-    console.log(error);
-  })
-  async addAsync<T = Record>(doc: T, path: string) {
+  @tryCatch(orLog)
+  async add<T = Record>(doc: T, path: string) {
     return await this.firestore.collection(path).add(doc);
   }
 
+  async addWithRef<T = Record>(doc: T, ref: Ref[] | string) {
+    if (doc instanceof LineItem && typeof ref === "string") {
+      doc.discount = ref;
+    } else if (doc instanceof Quotation && Array.isArray(ref)) {
+    }
+  }
+
   // Deletes path if the ref is not undefined
-  @tryCatch((error) => {
-    console.log(error);
-  })
-  async deleteAsync(path: string, ref?: string) {
+  @tryCatch(orLog)
+  async delete(path: string, ref?: string) {
     if (ref) {
       return await this.firestore.collection(path).doc(ref).delete();
     } else {
       return await this.firestore.doc(path).delete();
     }
+  }
+
+  @tryCatch(orLog)
+  async update<T = Record>(doc: T, path: string) {
+    return await this.firestore.doc(path).set(doc);
   }
 }
--- tests/unit/firestore.service.spec.ts
+++ tests/unit/firestore.service.spec.ts
@@ -1,7 +1,8 @@
-import { User } from "@/models";
+import { Client, Discount, LineItem, Quotation, Record, User } from "@/models";
+import { PRODUCT } from "@/models/LineItem";
 import { ADMIN_ROLE, BASE_ROLE } from "@/models/User";
 import FireService from "@/services/firestore.service";
-import { assertSucceeds } from "@firebase/testing";
+import { assertSucceeds, firestore } from "@firebase/testing";
 import { initialize, setup } from "./firebase-testing";
 
 const db = new FireService();
@@ -24,37 +25,98 @@ const user: User = {
   lastname: "example",
 };
 
-const setId = async (doc: any, obj: any) => {
-  obj.id = doc.id;
-  await db.firestore.collection("added").add({ doc });
+const client: Client = {
+  firstname: "test-client",
+  lastname: "example",
+  email: "test-client@gmail.com",
+  phone: "081-234-5746",
 };
 
-initialize()
+const discount: Discount = {
+  amount: 0.15,
+  details: "VAT Discount",
+  percentage: true,
+};
+
+const item: LineItem = {
+  type: PRODUCT,
+  cost: 1250,
+  units: 1,
+  details: "GoodYear 16in",
+  quantity: 1,
+};
+
+const draft: Quotation = {
+  user: "",
+  client: "",
+};
+
+const setId = async (
+  doc: firebase.firestore.DocumentReference,
+  obj: Record
+) => {
+  await db.firestore.collection("added").add({ doc });
+  obj.id = doc.id;
+  return doc;
+};
+
+const addObj = async (obj: Record, path: string) => {
+  const doc = await db.add(obj, path);
+  return await setId(doc, obj);
+};
+
+const removeObj = async (obj: Record, path: string) => {
+  await db.delete(path, obj.id);
+  if (obj.id) await db.firestore.collection("deleted").add(obj);
+};
+
+initialize();
 
 describe("Firebase Service", () => {
   beforeAll(async () => (db.firestore = await setup()));
 
   it("can add simple record object", async () => {
-    await assertSucceeds(
-      db.addAsync(admin, "users").then(async (doc) => await setId(doc, admin))
-    );
+    await assertSucceeds(addObj(admin, "users"));
+    await assertSucceeds(addObj(client, "clients"));
   });
 
   it("can delete simple record object", async () => {
-    let del = db.deleteAsync("users", admin.id).then(async () => {
-      if (admin.id) await db.firestore.collection("deleted").add(admin);
-    });
-
-    let add = db
-      .addAsync(user, "users")
-      .then(async (doc) => await setId(doc, user));
-
-    await assertSucceeds(Promise.all([del, add]));
+    await assertSucceeds(
+      Promise.all([removeObj(admin, "users"), addObj(user, "users")])
+    );
 
     await assertSucceeds(
-      db.deleteAsync(`users/${user.id}`).then(async () => {
+      db.delete(`users/${user.id}`).then(async () => {
         if (user.id) await db.firestore.collection("deleted").add(user);
       })
     );
+  });
+
+  it("can add complex/record object with ref objects", async () => {
+    const addDiscount = addObj(discount, "discounts").then(async (ref) => {
+      item.discount = ref;
+    });
+
+    await Promise.all([
+      db.update(admin, `users/${admin.id}`),
+      db.update(client, `clients/${client.id}`),
+      addDiscount,
+    ]);
+
+    const items = Promise.all([
+      addObj(item, "line-items"),
+      addObj(item, "line-items"),
+    ]).then(async (refs) => {
+      if (admin.id && client.id) {
+        draft.user = admin.id;
+        draft.client = client.id;
+      }
+      draft.items = refs;
+      draft.created_at = firestore.Timestamp.now();
+      draft.updated_at = firestore.Timestamp.now();
+      return await addObj(draft, "quotations");
+    });
+
+    await assertSucceeds(items);
   });
 });
