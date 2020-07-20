fdc557fbcb61bf843b9a460b1044ff6c8a999512
Ian Mubangizi <io@ianmubangizi.com>
2020-07-20 22:08:32 +0200

    added firebase doc class types with base record

--- src/models/Quotation.ts
+++ src/models/Quotation.ts
@@ -1,11 +1,11 @@
-import firebase from "firebase/app";
-import Record from "./Record";
+import Record, { CollectionRef, DocumentRef, Timestamp } from "./Record";
+
 export default class Quotation extends Record {
-  user!: firebase.firestore.DocumentReference | string; // User doc ref
-  client!: firebase.firestore.DocumentReference | string; // Client doc ref
-  items?: firebase.firestore.CollectionReference | string[]; // Array of LineItem doc ref
-  total!: number;
-  completed!: boolean;
-  created_at!: firebase.firestore.Timestamp;
-  updated_at!: firebase.firestore.Timestamp;
+  user!: DocumentRef; // User doc ref
+  client!: DocumentRef; // Client doc ref
+  items?: CollectionRef; // Array of LineItem doc ref
+  total?: number = 0.0;
+  completed?: boolean = false;
+  created_at?: Timestamp;
+  updated_at?: Timestamp;
 }
