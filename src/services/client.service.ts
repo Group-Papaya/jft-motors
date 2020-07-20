16650e65dbc29494184fee94ffc9fea28f711997
Ian Mubangizi <io@ianmubangizi.com>
2020-07-20 14:30:54 +0200

    removing firebase-storm
    The extra typing that cames with this package is not worth it, as we have spent more time debugging it's interface with firebase and we may lose less time if we just rewrote directly to firebase with it's api

--- src/services/client.service.ts
+++
@@ -1,9 +0,0 @@
-import { Client } from "@/models";
-import { catchError } from "@/utils";
-import { Collection } from "firebase-firestorm";
-
-export default {
-  saveClientAsync: (client: Client) =>
-    catchError(Collection(Client).create(client)),
-  deleteClientAsync: (id: string) => catchError(Collection(Client).remove(id))
-};
--- src/services/user.service.ts
+++
@@ -1,8 +0,0 @@
-import { User } from "@/models";
-import { Collection } from "firebase-firestorm";
-import { catchError } from "./../utils";
-
-export default {
-  saveUserAsync: (user: User) => catchError(Collection(User).create(user)),
-  deleteUserAsync: (id: string) => catchError(Collection(User).remove(id))
-};
