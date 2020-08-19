e3619981991cb2d17a516ba2233eff594cd0f37e
Ian Mubangizi <io@ianmubangizi.com>
2020-08-19 16:01:36 +0200

    user material

--- src/plugins/base.ts
+++ src/plugins/base.ts
@@ -2,7 +2,11 @@ import camelCase from "lodash/camelCase";
 import upperFirst from "lodash/upperFirst";
 import Vue from "vue";
 
-const requireComponent = require.context("@/components/", true, /\.vue$/);
+const requireComponent = require.context(
+  "@/components/material/",
+  true,
+  /\.vue$/
+);
 
 requireComponent.keys().forEach(fileName => {
   const componentConfig = requireComponent(fileName);
