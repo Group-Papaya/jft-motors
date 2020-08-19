f8d9c11cad3ea0b755f311913f9f523af1ece03f
Ian Mubangizi <io@ianmubangizi.com>
2020-08-19 21:11:44 +0200

    updated app, db - added firebase types

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,17 +1,18 @@
-import * as firebase from "firebase/app";
+import firebase from "firebase/app";
 import "firebase/auth";
+import "firebase/firestore";
 
 const configOptions = {
-  apiKey: "AIzaSyB1MvKFeYVIqnRBAmWIOrAbiiNFVl5kU1s",
-  authDomain: "jft-motors.firebaseapp.com",
-  databaseURL: "https://jft-motors.firebaseio.com",
-  projectId: "jft-motors",
-  storageBucket: "jft-motors.appspot.com",
-  messagingSenderId: "831362152534",
-  appId: "1:831362152534:web:1c8eff64629aefae07e3f7",
-  measurementId: "G-MQPC5KC6LV"
+    apiKey: "AIzaSyB1MvKFeYVIqnRBAmWIOrAbiiNFVl5kU1s",
+    authDomain: "jft-motors.firebaseapp.com",
+    databaseURL: "https://jft-motors.firebaseio.com",
+    projectId: "jft-motors",
+    storageBucket: "jft-motors.appspot.com",
+    messagingSenderId: "831362152534",
+    appId: "1:831362152534:web:1c8eff64629aefae07e3f7",
+    measurementId: "G-MQPC5KC6LV"
 };
 
-const db = firebase.initializeApp(configOptions);
-export default db;
-//   fb.analytics()
+// Get a App instance
+export const app = firebase.initializeApp(configOptions);
+export const db = app.firestore();
--- src/firestore.ts
+++
@@ -1,7 +0,0 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
-
-// Get a Firestore instance
-export const firestore = firebase
-  .initializeApp({ projectId: "jft-motors" })
-  .firestore();
