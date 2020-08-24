7c8030b71340a3c4913ea0bb02495b04f3834625
Ian Mubangizi <io@ianmubangizi.com>
2020-08-24 23:08:39 +0200

    Document Data watchers

--- src/services/curd.service.ts
+++ src/services/curd.service.ts
@@ -1,1 +1,13 @@
-export default {};
+import { dbService } from "./firestore.service";
+export default {
+  watchCollection(fn: (data: any) => any, path: string) {
+    return dbService.getCollection(path).onSnapshot(querySnapshot => {
+      fn(querySnapshot.docs.map(snapshot => snapshot.data()));
+    });
+  },
+  watchDocument(fn: (data: any) => any, path: string, ref?: string) {
+    return dbService.getDocument(path, ref).onSnapshot(snapshot => {
+      fn(snapshot.data());
+    });
+  }
+};
