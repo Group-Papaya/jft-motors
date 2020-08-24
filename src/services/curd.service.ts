f011d2f23921c52bae47d50bce9637879b502992
Ian Mubangizi <io@ianmubangizi.com>
2020-08-24 23:45:53 +0200

    basic curd funcations

--- src/services/curd.service.ts
+++ src/services/curd.service.ts
@@ -1,3 +1,4 @@
+import { Record } from "@/models";
 import { dbService } from "./firestore.service";
 export default {
   watchCollection(fn: (data: any) => any, path: string) {
@@ -9,5 +10,22 @@ export default {
     return dbService.getDocument(path, ref).onSnapshot(snapshot => {
       fn(snapshot.data());
     });
+  }
+};
+
+export const curd = {
+  async get(path: string, ref?: string) {
+    return await dbService
+      .getSnapshot(path, ref)
+      .then(snapshot => snapshot.data());
+  },
+  add<T = Record>(record: T, path: string, ref?: string) {
+    return dbService.add(record, path, ref);
+  },
+  update<T = Record>(record: T, path: string, ref?: string) {
+    return dbService.setDocument(record, dbService.getDocument(path, ref));
+  },
+  delete(path: string) {
+    return dbService.delete(path);
   }
 };
