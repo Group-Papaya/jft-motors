fbbfc2d7e87767ba760bb157520e8309a8e23f12
Michael Lungu <mikeaxle2006@gmail.com>
2020-08-27 13:27:39 +0200

    Update QuotationEditor.vue

--- src/views/QuotationEditor.vue
+++ src/views/QuotationEditor.vue
@@ -4,125 +4,131 @@
     <v-row class="mb-8 flex-row flex-sx-column">
       <v-col cols="9">
         <v-row col="12" class="justify-md-center ml-1">
-          <v-btn width="100" color="warning">Draft</v-btn>
-          <v-btn width="100">Complete</v-btn>
+          <v-btn width="100" color="warning" >Draft</v-btn>
+          <v-btn width="100" light @click="markComplete()">Complete</v-btn>
         </v-row>
       </v-col>
       <v-col cols="3">
         <v-row col="12" class="justify-end mr-1">
-          <v-btn color="warning">Delete Quotation</v-btn>
+          <v-btn color="warning" class="d-none d-sm-flex"  @click="deleteQuotation()">Delete Quotation</v-btn>
+            <v-btn fab right x-small color="warning" class="d-flex d-sm-none" @click="addLineItem()"><v-icon>mdi-delete</v-icon></v-btn>
         </v-row>
       </v-col>
     </v-row>
 
     <!--  page   -->
-    <app-material-card
-      color="warning"
-      icon="mdi-note"
-      :title="'Quotation ' + quotationId"
-      class="px-5 py-3"
-    >
-      <div class="px-md-16">
-        <!-- quotation header     -->
-        <v-col col="12">
-          <v-row>
-            <v-col>
-              <div class="text-caption font-weight-bold">Quotation #</div>
-              <div class="text-subtitle-1" v-text="quotation.id"></div>
-            </v-col>
-            <v-col>
-              <div class="text-caption font-weight-bold">Client</div>
-              <div class="text-subtitle-1" v-text="quotation.client"></div>
-            </v-col>
-            <v-col>
-              <div class="text-caption font-weight-bold">Prepared By</div>
-              <div class="text-subtitle-1" v-text="quotation.user"></div>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col>
-              <div class="text-caption font-weight-bold">Created</div>
-              <div class="text-subtitle-1" v-text="quotation.created"></div>
-            </v-col>
-            <v-col>
-              <div class="text-caption font-weight-bold">Modified</div>
-              <div class="text-subtitle-1" v-text="quotation.updated"></div>
-            </v-col>
-            <v-col>
-              <div class="text-caption font-weight-bold"></div>
-              <div class="text-subtitle-1"></div>
-            </v-col>
-          </v-row>
-        </v-col>
+      <app-material-card
+                  color="warning"
+                  icon="mdi-note"
+                  :title="'Quotation ' + quotationId"
+                  max-width="800px"
+                  class="px-5 py-3 mx-md-auto">
+              <div class="px-md-10">
+                  <!-- quotation header     -->
+                  <v-col col="12">
+                      <v-row>
+                          <v-col>
+                              <div class="caption font-weight-bold">Quotation #</div>
+                              <div class="body-2" v-text="quotation.id"></div>
+                          </v-col>
+                          <v-col>
+                              <div class="caption font-weight-bold">Client</div>
+                              <div class="body-2" v-text="quotation.client"></div>
+                          </v-col>
+                          <v-col>
+                              <div class="caption font-weight-bold">Prepared By</div>
+                              <div class="body-2" v-text="quotation.user"></div>
+                          </v-col>
+                      </v-row>
+                      <v-row>
+                          <v-col>
+                              <div class="caption font-weight-bold">Created</div>
+                              <div class="body-2" v-text="quotation.created"></div>
+                          </v-col>
+                          <v-col>
+                              <div class="caption font-weight-bold">Modified</div>
+                              <div class="body-2" v-text="quotation.updated"></div>
+                          </v-col>
+                          <v-col>
+                              <div class="caption font-weight-bold"></div>
+                              <div class="body-2"></div>
+                          </v-col>
+                      </v-row>
+                  </v-col>
 
-        <hr class="my-3" />
+                  <v-divider class="my-4" light></v-divider>
 
-        <v-row col="12" class="justify-end align-center">
-          <v-btn @click="addLineItem()" color="warning">Add Line Item</v-btn>
-        </v-row>
+                  <v-row col="12" class="justify-end align-center">
+                      <v-btn class="d-none d-sm-flex" @click="addLineItem()" color="warning">Add Line Item</v-btn>
+                      <v-btn fab right x-small color="warning" class="d-flex d-sm-none" @click="addLineItem()"><v-icon>mdi-plus</v-icon></v-btn>
+                  </v-row>
 
-        <hr class="my-3" />
+                  <v-divider class="my-4" light></v-divider>
 
-        <!-- quotation line items -->
-        <v-simple-table>
-          <template v-slot:default>
-            <thead>
-              <tr>
-                <th class="text-left">#</th>
-                <th class="text-left">Name</th>
-                <th class="text-left">Qty</th>
-                <th class="text-left">Discount</th>
-                <th class="text-left">Cost</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="(item, index) in quotation.items" :key="item.id">
-                <td>{{ index + 1 }}</td>
-                <td>{{ item.name }}</td>
-                <td>{{ item.quantity }}</td>
-                <td>
-                  {{
-                    (item.discounted ? item.discount : "0") | currency("R", 2)
-                  }}
-                </td>
-                <td>{{ item.cost | currency("R", 2) }}</td>
-              </tr>
-              <tr>
-                <td class="font-weight-bold">Sub Total</td>
-                <td></td>
-                <td></td>
-                <td class="font-weight-bold red--text">
-                  -{{ discountTotal | currency("R", 2) }}
-                </td>
-                <td class="font-weight-bold">{{ total | currency("R") }}</td>
-              </tr>
-              <tr>
-                <td class="font-weight-bold">Discount</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td class="font-weight-bold red--text">
-                  -{{ quotationDiscount | currency("R", 2) }}
-                </td>
-              </tr>
-              <tr>
-                <td class="font-weight-bold">Total</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td class="font-weight-bold">
-                  {{
-                    (total - discountTotal - quotationDiscount)
-                      | currency("R", 2)
-                  }}
-                </td>
-              </tr>
-            </tbody>
-          </template>
-        </v-simple-table>
-      </div>
-    </app-material-card>
+                  <!-- quotation line items -->
+                  <v-simple-table>
+                      <template v-slot:default>
+                          <thead>
+                          <tr>
+                              <th class="text-left">#</th>
+                              <th class="text-left">Name</th>
+                              <th class="text-left">Qty</th>
+                              <th class="text-left">Discount</th>
+                              <th class="text-left">Cost</th>
+                          </tr>
+                          </thead>
+                          <tbody>
+                          <tr v-for="(item, index) in quotation.items" :key="item.id">
+                              <td>{{ index + 1 }}</td>
+                              <td>{{ item.name }}</td>
+                              <td>{{ item.quantity }}</td>
+                              <td>
+                                  {{
+                                  (item.discounted ? item.discount : "0") | currency("R", 2)
+                                  }}
+                              </td>
+                              <td>{{ item.cost | currency("R", 2) }}</td>
+                          </tr>
+                          <tr>
+                              <td></td>
+                              <td class="font-weight-bold">Sub Total</td>
+                              <td></td>
+                              <td class="font-weight-bold red--text">
+                                  -{{ discountTotal | currency("R", 2) }}
+                              </td>
+                              <td class="font-weight-bold">{{ total | currency("R") }}</td>
+                          </tr>
+                          <tr>
+                              <td></td>
+                              <td class="font-weight-bold">Discount</td>
+                              <td></td>
+                              <td></td>
+                              <td class="font-weight-bold red--text">
+                                  -{{ quotationDiscount | currency("R", 2) }}
+                              </td>
+                          </tr>
+                          <tr>
+                              <td></td>
+                              <td class="font-weight-bold">Total</td>
+                              <td></td>
+                              <td></td>
+                              <td class="font-weight-bold">
+                                  {{
+                                  (total - discountTotal - quotationDiscount)
+                                  | currency("R", 2)
+                                  }}
+                              </td>
+                          </tr>
+                          </tbody>
+                      </template>
+                  </v-simple-table>
+              </div>
+          </app-material-card>
   </v-container>
+    <v-dialog v-model="addLineItemDialog">
+
+    </v-dialog>
+
 </template>
 
 <script lang="ts">
@@ -135,6 +141,7 @@ import { PRODUCT, WORKER } from "@/models/LineItem";
 })
 export default class QuotationEditor extends Mixins() {
   name = "QuotationEditor";
+  addLineItemDialog = false;
   quotationId = "";
   quotation = new Quotation();
   quotationDiscount = 10;
@@ -162,6 +169,14 @@ export default class QuotationEditor extends Mixins() {
   addLineItem() {
     console.log("open modal");
   }
+
+    deleteQuotation() {
+      console.log(this.quotationId)
+    }
+
+    markComplete() {
+      console.log("completed")
+    }
 
   get discountTotal() {
     return 12;
