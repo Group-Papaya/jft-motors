7ca55d86ddeb58a3ce6f2efe7234221353f8ba2b
Ian Mubangizi <io@ianmubangizi.com>
2020-08-30 16:15:43 +0200

    add and remove a document snapshot path
    Used for tracking it's firebase path later on in the application

--- src/services/curd.service.ts
+++ src/services/curd.service.ts
@@ -1,14 +1,24 @@
 import { Record } from "@/models";
 import { dbService } from "./firestore.service";
+
+const snap = (snapshot: { data: () => any; ref: { path: any } }) => {
+  return { ...snapshot.data(), snapPath: snapshot.ref.path };
+};
+
+const unsnap = (object: any) => {
+  delete object.snapPath;
+  return object;
+};
+
 export default {
   watchCollection(fn: (data: any) => any, path: string) {
     return dbService.getCollection(path).onSnapshot(querySnapshot => {
-      fn(querySnapshot.docs.map(snapshot => snapshot.data()));
+      fn(querySnapshot.docs.map(snapshot => snap(snapshot)));
     });
   },
   watchDocument(fn: (data: any) => any, path: string, ref?: string) {
     return dbService.getDocument(path, ref).onSnapshot(snapshot => {
-      fn(snapshot.data());
+      fn(snap(snapshot));
     });
   }
 };
@@ -17,13 +27,16 @@ export const curd = {
   async get(path: string, ref?: string) {
     return await dbService
       .getSnapshot(path, ref)
-      .then(snapshot => snapshot.data());
+      .then(snapshot => snap(snapshot));
   },
   add<T = Record>(record: T, path: string, ref?: string) {
     return dbService.add(record, path, ref);
   },
   update<T = Record>(record: T, path: string, ref?: string) {
-    return dbService.setDocument(record, dbService.getDocument(path, ref));
+    return dbService.setDocument(
+      unsnap(record),
+      dbService.getDocument(path, ref)
+    );
   },
   delete(path: string) {
     return dbService.delete(path);
