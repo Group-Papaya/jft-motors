674fddd69bec588ebea64ad8ea27bc0cdbec1723
Michael Lungu <mikeaxle2006@gmail.com>
2020-09-22 22:33:50 +0200

    view pdf function

---
+++ src/components/layouts/AppPdfViewer.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-dialog v-model="dialog" v-if="dialog" max-width="1200">
+    <v-card>
+      <v-card-title>Add Line Item to Quotation</v-card-title>
+      <v-card-text>
+        <div id="pdf"></div>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: "AppPdfViewer",
+  data() {
+    return {
+      dialog: false
+    };
+  },
+  methods: {
+    toggleDialog() {
+      this.dialog = !this.dialog;
+    }
+  }
+};
+</script>
+
+<style scoped></style>
