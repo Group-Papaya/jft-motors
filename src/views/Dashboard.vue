be28cf69007a38d6368931fa085b80544e825461
Michael Lungu <mikeaxle2006@gmail.com>
2020-09-29 15:29:17 +0200

    fix:  full height list cards

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -113,26 +113,4 @@ export default class Dashboard extends Vue {
 }
 </script>
 
-<style scoped lang="scss">
-.hello {
-  text-align: center;
-}
-
-h3 {
-  margin: 40px 0 0;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-</style>
+<style scoped lang="scss"></style>
