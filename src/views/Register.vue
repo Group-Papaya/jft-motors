29f581ee05774f0db103f8f79d7bd54ce521fd83
Ian Mubangizi <io@ianmubangizi.com>
2020-09-30 12:25:09 +0200

    Made major changes to the auth services
    - checked the the UserCredentials have a valid user
    - the socialLogin function now requires the callback
    
    Changes made to the Register.vue
    - Added information collected from socialLogin
    - Registering a default user with the MANAGER_ROLE

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -6,7 +6,45 @@
     button-text="Submit Details"
     title="Setup Your Business Account"
   >
-    <v-col v-if="valid">
+    <v-col class="mt-n6" v-if="valid">
+      <v-col class="col-12 mb-n3">
+        <v-row>
+          <v-col cols="6">
+            <v-text-field
+              required
+              :disabled="disabled.firstname"
+              type="text"
+              :rules="[v => !!v || `Your name is required`]"
+              label="Firstname"
+              class="purple-input"
+              v-model="form.firstname"
+            />
+          </v-col>
+          <v-col cols="6">
+            <v-text-field
+              :disabled="disabled.lastname"
+              required
+              type="text"
+              label="Lastname"
+              class="purple-input"
+              :rules="[v => !!v || `Your Lastname is required`]"
+              v-model="form.lastname"
+            />
+          </v-col>
+          <v-col class="col-12 mt-n6">
+            <v-text-field
+              :disabled="disabled.email"
+              required
+              type="text"
+              :rules="[v => !!v || `Your Email is required`]"
+              label="Email"
+              class="purple-input"
+              v-model="form.email"
+            />
+          </v-col>
+        </v-row>
+      </v-col>
+
       <v-col class="col-12">
         <v-text-field
           required
@@ -42,9 +80,9 @@
       <p class="text-center">
         Sign In to Continue<br />
         <img
-          @click="login()"
           alt="Google Logo"
           class="social-button"
+          @click="login(form, disabled)"
           src="../assets/google-logo.png"
         />
       </p>
@@ -55,8 +93,8 @@
 <script>
 import { auth } from "@/services/auth.service";
 import AppAuth from "@/components/layouts/AppAuth";
-import { MANAGER_ROLE } from "@/models/User";
 import { db } from "@/firebase";
+import { MANAGER_ROLE } from "@/models/User";
 
 export default {
   name: "Register",
@@ -64,16 +102,24 @@ export default {
   data() {
     return {
       valid: false,
+      disabled: {
+        email: false,
+        firstname: false,
+        lastname: false
+      },
       form: {
+        email: "",
         company: "",
         address: "",
-        telephone: ""
+        telephone: "",
+        lastname: "",
+        firstname: ""
       },
       rules: {
         address: [
           v => !!v || "Business Address is required",
           v =>
-            /^([\d]{0,5})(([\w\s]*,){2,3})(\s[\d]{4},)(.)(South Africa)/gm.test(
+            /^([\d]{0,5})(([\w\s]*,){2,3})(\s[\d]{4},)(.)(South Africa)$/gm.test(
               v
             ) ||
             "Address should have: Street Address, Suburb, City, Postal Code, South Africa"
@@ -89,16 +135,43 @@ export default {
     };
   },
   methods: {
-    async login() {
-      await auth.socialLogin(MANAGER_ROLE).then(value => {
-        if (value.result) {
-          this.valid = true;
-          this.$toast.success("Logged in with Google");
-        } else this.$toast.error("Failed to use Google");
-      });
+    async login(form, disabled) {
+      await auth
+        .socialLogin(function(credentials) {
+          const { user } = credentials;
+          const {
+            given_name: firstname,
+            family_name: lastname
+          } = credentials.additionalUserInfo.profile;
+
+          if (firstname && lastname) {
+            form.lastname = lastname;
+            form.firstname = firstname;
+            disabled.firstname = true;
+            disabled.lastname = true;
+          }
+
+          if (user.email) {
+            disabled.email = true;
+            form.email = user.email;
+          }
+        })
+        .then(value => {
+          if (value.result) {
+            this.valid = true;
+            this.$toast.success("Logged in with Google");
+          } else this.$toast.error("Failed to use Google");
+        });
     },
     async submitDetails() {
-      const { address, telephone, company } = this.form;
+      const {
+        email,
+        address,
+        company,
+        lastname,
+        firstname,
+        telephone
+      } = this.form;
       const details = address.split(",").map(it => it.trim());
       const last = details.length - 1;
       const newAddress = {
@@ -108,6 +181,15 @@ export default {
         zipcode: details[last - 1],
         country: details[last]
       };
+
+      await auth.register({
+        password: "P@ssword1",
+        role: MANAGER_ROLE,
+        firstname,
+        lastname,
+        email
+      });
+
       await db
         .collection("/settings")
         .doc("business-details")
